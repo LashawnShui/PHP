aeebf2bd539dcc1f563b217cd0ddb880f131864b
LashawnShui <42991113+LashawnShui@users.noreply.github.com>
2019-04-20 22:53:39 +0800

    Revert "Experiment2_2-Message Board"
    This reverts commit ac953678dd70aa86486b2a88f69ff9373d843e9e.

--- xycmslyb_v1.9/system/include/globle.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('XYCMS_BOOK', true);
-define('XYCMS_IN', str_replace('system/include/globle.inc.php', '', str_replace('\\', '/', __FILE__)));
-ini_set("magic_quotes_runtime",0);
-//set_magic_quotes_runtime(0);
-//session_start();
-require XYCMS_IN . 'Conf/xycms.inc.php';;
-require XYCMS_IN . 'Libs/Function/fun.php';
-?>
